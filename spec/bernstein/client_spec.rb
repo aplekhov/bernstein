4f846cfde9b65826cd0c0015876dd35560978398
Anthony Plekhov <anthony.plekhov@gmail.com>
2014-09-12 18:04:02 +0200

    added client spec

---
+++ spec/bernstein/client_spec.rb
@@ -0,0 +1,27 @@
+require 'helper'
+
+describe Bernstein::Client do
+  describe "initializing" do
+    it "should pass configuration values to persister module"
+  end
+
+  describe "sending a message" do
+    it "should build the message object and return its id" do
+      id = Bernstein::Client.send_message("/synths/4/chord/notes 25 30 10")
+      expect(id).to_not be_nil
+      queued_messages = Bernstein::Message.get_queued_messages
+      message = queued_messages.find{|m| m.id == id}
+      expect(message).to_not be_nil
+      expect(message.address).to eq("/synths/4/chord/notes")
+      expect(message.args).to eq(["25", "30", "10"])
+    end
+  end
+
+  describe "querying a message's status by id" do
+    it "should return the current status or not yet queued" do
+      id = Bernstein::Client.send_message("/synths/4/chord/notes 25 30 10")
+      expect(Bernstein::Client.message_status(id)).to eq(Bernstein::Persistence::STATES[:queued])
+      expect(Bernstein::Client.message_status('123')).to eq(Bernstein::Persistence::STATES[:not_yet_queued])
+    end
+  end
+end
