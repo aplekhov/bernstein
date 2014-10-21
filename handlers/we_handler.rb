fb62d2b66cf0de74be43377dd42619347fd86500
Anthony Plekhov <anthony.plekhov@gmail.com>
2014-10-21 23:47:50 +0200

    cleanup

--- handlers/we_handler.rb
+++
@@ -1,8 +0,0 @@
-class Test2Handler
-  def process_requests(requests, request_manager)
-    puts "Test2Handler: processing requests: #{requests.inspect}"
-    requests.each do |r|
-      request_manager.send_request(r["id"], "/#{r["params"].keys.first}", r["params"].values.first)
-    end
-  end
-end
